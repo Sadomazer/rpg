69b7b4e4989f6de834e0c24d44d44fc33d4afa94
Sadomazer <valentin_solntsev@mail.ru>
2021-07-03 23:25:22 +0300

    correction

--- src/index.js
+++ src/index.js
@@ -6,14 +6,21 @@ const ctx = canvas.getContext('2d');
 const spriteW = 48;
 const spriteH = 48;
 const shots = 3;
+const step = 10;
+const charSide = {
+  down: 0,
+  up: 144,
+  right: 96,
+  left: 48,
+};
+let view = charSide.down;
 let cycle = 0;
-let pY = canvas.height / 2 - 48;
-let pX = canvas.width / 2 - 48;
+let pY = canvas.height / 2 - spriteH;
+let pX = canvas.width / 2 - spriteW;
 let downPressed = false;
 let upPressed = false;
 let rightPressed = false;
 let leftPressed = false;
-let view = 0;
 
 function keyDownHandler(e) {
   if (e.key === 'Down' || e.key === 'ArrowDown') {
@@ -52,47 +59,43 @@ const img = document.createElement('img');
 img.src = SenseiWalk;
 
 function moveLimiter() {
-    if (pY + 48 > canvas.height) {
-        pY = canvas.height - 48;
-    }
-    if (pX + 48 > canvas.width) {
-        pX = canvas.width - 48;
-    }
-    if (pY < 0) {
-        pY = 0;
-    }
-    if (pX < 0) {
-        pX = 0;
-    }
+  if (pY + spriteH > canvas.height) {
+    pY = canvas.height - spriteH;
+  }
+  if (pX + spriteW > canvas.width) {
+    pX = canvas.width - spriteW;
+  }
+  if (pY < 0) {
+    pY = 0;
+  }
+  if (pX < 0) {
+    pX = 0;
+  }
 }
 
 img.addEventListener('load', () => {
   setInterval(() => {
     if (downPressed) {
-      pY += 10;
-      cycle = (cycle + 1) % shots;
-      view = 0;
+      pY += step;
+      view = charSide.down;
     }
     if (upPressed) {
-      pY -= 10;
-      cycle = (cycle + 1) % shots;
-      view = 144;
+      pY -= step;
+      view = charSide.up;
     }
     if (rightPressed) {
-      pX += 10;
-      cycle = (cycle + 1) % shots;
-      view = 96;
+      pX += step;
+      view = charSide.right;
     }
     if (leftPressed) {
-      pX -= 10;
-      cycle = (cycle + 1) % shots;
-      view = 48;
+      pX -= step;
+      view = charSide.left;
     }
 
+    cycle = (cycle + 1) % shots;
     moveLimiter();
 
     ctx.clearRect(0, 0, 600, 600);
-    ctx.drawImage(img, cycle * spriteW, view, spriteW, spriteH, pX, pY, 48, 48);
+    ctx.drawImage(img, cycle * spriteW, view, spriteW, spriteH, pX, pY, spriteH, spriteW);
   }, 80);
 });
-
